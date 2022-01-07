0ed228d851d206749ea12781c5f1132f83ea2446
rusbell ruiz <rusbell.ruiz.p@gmail.com>
2022-01-06 20:28:09 -0500

    elimnando archivos erroneos

--- Implementacion_Proyecto/preguntas-backend/app.js
+++
@@ -1,6 +0,0 @@
-require('dotenv').config();
-const Server = require("./models/server.js");
-
-const server = new Server();
-server.listen();
-
--- Implementacion_Proyecto/preguntas-backend/routes/jugador.js
+++
@@ -1,9 +0,0 @@
-const { Router } = require('express');
-const { getJugador } = require('../controller/jugador');
-
-const route = Router();
-
-route.get('/jugador', getJugador);
-
-
-module.exports = route;
